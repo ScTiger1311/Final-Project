81a52f1959c8134454b144257b7a71b676dff447
mrzebrasmoothie <averyweibel@yahoo.com>
2021-05-11 12:00:48 -0700

    Movement and Environment prototype

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ let config =
     height: 768,
     antialias: false,
     scene: [Menu, Play],
+    physics:{
+        default: 'arcade',
+        arcade: {
+            debug: false,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
     
 }
 
@@ -13,5 +23,5 @@ let game = new Phaser.Game(config);
 //set UI sizes
 
 //reserve keyboard bindings
-let keyF, keyR, keyLEFT, keyRIGHT;
+let keyF, keyR, keyLEFT, keyRIGHT, keyUP, keyDOWN, keySPACE;
 
