f3ba3485a53e01b1c3cb5daf04a7c5e47e826d06
Cameron Beattie <beattiecameron99@gmail.com>
2021-06-07 18:37:20 -0700

    added pause menu

--- src/scenes/PauseMenu.js
+++ src/scenes/PauseMenu.js
@@ -2,22 +2,35 @@ class PauseMenu extends Phaser.Scene{
     constructor(){
         super("PauseMenu");
     }
+
+    preload() {
+        this.load.image("pause_image", "./assets/single_sprites/Pause.png");
+
+        //3 frames, named button_neutral, button_hover, button_down
+        this.load.atlas("button", "./assets/spritesheets/button_spritesheet.png", "./assets/spritesheets/button_spritesheet.json");
+        this.load.atlas("Resume", "./assets/spritesheets/Resume_Atlas.png", "./assets/spritesheets/Resume_Atlas.json");
+        this.load.atlas("Reset", "./assets/spritesheets/Reset_Atlas.png", "./assets/spritesheets/Reset_Atlas.json");
+        this.load.atlas("Menu", "./assets/spritesheets/Menu_Atlas.png", "./assets/spritesheets/Menu_Atlas.json");
+    }
+
     create(){
+        this.pauseSprite = this.add.sprite(0, 0, "pause_image").setOrigin(0, 0);
+
         console.log("Paused the game.");
-        this.resumeButton = new Button(this, "button", 200, 200, function(){
+        this.resumeButton = new Button(this, "Resume", 200, 100, function(){
             var playing = this.scene.scene.get('Play');
             playing.music.setVolume(0.06);
             this.scene.scene.resume('Play');
             this.scene.scene.sleep();
             });
             
-        this.restartButton = new Button(this, "button", 300, 200, function(){
+        this.restartButton = new Button(this, "Reset", 224, 190, function(){
             var playing = this.scene.scene.get('Play');
             playing.music.stop();
             playing.scene.restart();
             this.scene.scene.sleep();
         })
-        this.menuButton = new Button(this, "button", 400, 200, function(){
+        this.menuButton = new Button(this, "Menu", 224, 280, function(){
             var playing = this.scene.scene.get('Play');
             playing.music.stop();
             playing.scene.stop();
